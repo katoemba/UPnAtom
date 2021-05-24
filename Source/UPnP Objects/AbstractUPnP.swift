1c7407ddfc464570b741b4a930a57921d5dcecaa
Berrie Kremers <berrie.kremers@xs4all.nl>
2021-05-24 17:49:10 +0200

    Compile for both iOS and macOS.

--- Source/UPnP Objects/AbstractUPnP.swift
+++ Source/UPnP Objects/AbstractUPnP.swift
@@ -69,14 +69,10 @@ extension AbstractUPnP {
 }
 
 extension AbstractUPnP: ExtendedPrintable {
-    public var className: String {
-        return "\(type(of: self))"
-    }
-    
     #if os(iOS)
-//    public var className: String { return "\(type(of: self))" }
+    public var className: String { return "\(type(of: self))" }
     #elseif os(OSX) // NSObject.className actually exists on OSX! Who knew.
-//    override public var className: String { return "\(type(of: self))" }
+    override public var className: String { return "\(type(of: self))" }
     #endif
     override public var description: String {
         var properties = PropertyPrinter()
